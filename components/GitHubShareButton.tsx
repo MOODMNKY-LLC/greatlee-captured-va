17eee1451eb464922db889b06668db484647d0ce
MOOD MNKY <137089846+MOODMNKY-LLC@users.noreply.github.com>
2024-01-18 11:56:10 -0500

    bot install

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -16,7 +16,7 @@ export const GitHubShareButton: React.FC = () => {
         height='80'
         viewBox='0 0 250 250'
         style={{
-          fill: '#D1E4DE',
+          fill: '#99C9A3',
           color: '#fff',
           position: 'absolute',
           zIndex: 1001,
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react'
 import type { AppProps } from 'next/app'
 import { useRouter } from 'next/router'
+import { BubbleChat } from 'flowise-embed-react'; // Import BubbleChat
 
 import * as Fathom from 'fathom-client'
 // used for rendering equations (optional)
@@ -61,5 +62,49 @@ export default function App({ Component, pageProps }: AppProps) {
     }
   }, [router.events])
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <BubbleChat
+        chatflowid="2b9af05c-fcab-4470-92c2-5a6e90467d2c"
+        apiHost="https://litlaw-flowise.moodmnky.com"
+        theme={{
+          button: {
+            backgroundColor: "#193F4E",
+            right: 20,
+            bottom: 20,
+            size: "medium",
+            iconColor: "white",
+            customIconSrc: "https://cdn.shopify.com/s/files/1/0693/4328/1426/files/moodmnky-flowise-react-icon-seafoam.svg",
+          },
+          chatWindow: {
+            welcomeMessage: "Welcome back Lee! How can I be of service?",
+            backgroundColor: "#99C9A3",
+            height: 700,
+            width: 400,
+            fontSize: 16,
+            poweredByTextColor: "#99C9A3",
+            botMessage: {
+              backgroundColor: "#2F3437",
+              textColor: "#FFFFFF",
+              showAvatar: true,
+              avatarSrc: "https://cdn.discordapp.com/attachments/1083532452347269220/1192490146281574442/litlaw-avatar-headshot2.png",
+            },
+            userMessage: {
+              backgroundColor: "#539997",
+              textColor: "#ffffff",
+              showAvatar: true,
+              avatarSrc: "https://raw.githubusercontent.com/zahidkhawaja/langchain-chat-nextjs/main/public/usericon.png",
+            },
+            textInput: {
+              placeholder: "Type your question",
+              backgroundColor: "#2F3437",
+              textColor: "#ffffff",
+              sendButtonColor: "#99C9A3",
+            }
+          }
+        }}
+      />
+  <Component {...pageProps} />
+  </>
+  );
 }
